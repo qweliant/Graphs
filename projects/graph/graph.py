fbac0a72b93ff7ab994067a9e577c3ed9627102e
qweliant <qweliantanner@gmail.com>
2020-03-17 06:16:07 -0400

    bfs

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,36 +3,69 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
+class Queue:
+    def __init__(self):
+        self.storage = []
+
+    def enqueue(self, value):
+        self.storage.append(value)
+
+    def dequeue(self):
+        if len(self.storage) > 0:
+            return self.storage.pop(0)
+        else:
+            return None
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex_id):
+    def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
 
-    def add_edge(self, v1, v2):
+    def add_edge(self, vertex, edge):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # # now this should throw an error if i am not checking keys
+        # self.vertices[vertex].add(edge) 
+        if vertex in self.vertices and edge in self.vertices:
+
+            self.vertices[vertex].add(edge)
+
+        else:
+
+            self.add_vertex(vertex)
+            self.vertices[vertex].add(edge) 
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        print(self.vertices)
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_node)
+        while len(q.storage) > 0:
+            node = q.dequeue()
+            visited.add(node)
+            print(node)
+            for child in self.vertices[node]:
+                if child not in visited and child not in q.storage:
+                    q.enqueue(child)
+
 
     def dft(self, starting_vertex):
         """
@@ -120,26 +153,26 @@ if __name__ == '__main__':
     '''
     graph.bft(1)
 
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft(1)
-    graph.dft_recursive(1)
+    # '''
+    # Valid DFT paths:
+    #     1, 2, 3, 5, 4, 6, 7
+    #     1, 2, 3, 5, 4, 7, 6
+    #     1, 2, 4, 7, 6, 3, 5
+    #     1, 2, 4, 6, 3, 5, 7
+    # '''
+    # graph.dft(1)
+    # graph.dft_recursive(1)
 
-    '''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-    print(graph.bfs(1, 6))
+    # '''
+    # Valid BFS path:
+    #     [1, 2, 4, 6]
+    # '''
+    # print(graph.bfs(1, 6))
 
-    '''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # '''
+    # Valid DFS paths:
+    #     [1, 2, 4, 6]
+    #     [1, 2, 4, 7, 6]
+    # '''
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
