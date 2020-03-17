72b7edb0aeed7903b6ec14d283080b872cd4e698
qweliant <qweliantanner@gmail.com>
2020-03-17 14:12:23 -0400

    noice

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,17 @@
+import sys
+sys.path.append('../')
+
+from graph.graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    graph = Graph()
+    
+    for a,b in ancestors:
+        print(ancestors[0])
+
+
+
+
+    
